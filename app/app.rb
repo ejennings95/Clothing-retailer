d39566c6023715a1c07d51061dbd4fdfee7461a9
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-04-12 13:12:21 +0100

    :sparkles: adds item to cart when button clicked

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,18 @@ require 'sinatra/base'
 require_relative '../lib/shoppingcart.rb'
 
 class ClothingRetailer < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    @shoppingcart = ShoppingCart.new
+    session[:shoppingcart] ||= ShoppingCart.new
+    @shoppingcart = session[:shoppingcart]
     erb(:index)
+  end
+
+  post '/add_to_cart' do
+    session[:shoppingcart].add_to_cart(params[:item_name])
+    p session[:shoppingcart].shoppingcart
+    redirect '/'
   end
 
     run! if app_file == $0
