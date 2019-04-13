676fb59ad93b86064958f0d894e51b15453b7f9a
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-04-13 11:13:06 +0100

    :recycle: refactor privatise methods and remove testing these

--- lib/discountcodes.rb
+++ lib/discountcodes.rb
@@ -7,10 +7,10 @@ class DiscountCodes
 
   attr_reader :discount_attempted
 
-  def discount_check(code, shoppingcarttotal, shoppingcart)
+  def discount_check(code, shoppingcarttotal, shoppingcart, stock = Stock.new)
     return 5 if discount_five(code) == true
     return 10 if discount_ten(code, shoppingcarttotal) == true
-    return 15 if discount_fifteen(code, shoppingcarttotal, shoppingcart) == true
+    return 15 if discount_fifteen(code, shoppingcarttotal, shoppingcart, stock) == true
     0
   end
 
@@ -18,6 +18,8 @@ class DiscountCodes
     return true if code == FIVE_OFF || code == TEN_OFF || code == FIFTEEN_OFF
     false
   end
+
+  private
 
   def discount_five(code)
     code == FIVE_OFF ? true : false
@@ -27,11 +29,9 @@ class DiscountCodes
     (code == TEN_OFF && shoppingcarttotal > 50) ? true : false
   end
 
-  def discount_fifteen(code, shoppingcarttotal, shoppingcart, stock = Stock.new)
+  def discount_fifteen(code, shoppingcarttotal, shoppingcart, stock)
     (code == FIFTEEN_OFF && shoppingcarttotal > 75 && footwearitem?(shoppingcart, stock)) ? true : false
   end
-
-  private
 
   def footwearitem?(shoppingcart, stock)
     shoppingcart.each{ | item | return true if stock.check_category(item, "Men's Footwear") }
--- spec/discountcodes_spec.rb
+++ spec/discountcodes_spec.rb
@@ -10,52 +10,6 @@ describe 'DiscountCodes' do
     @discountcodes = DiscountCodes.new
   end
 
-  context '5 pounds off' do
-    it 'should return true if the code is correct' do
-      expect(@discountcodes.discount_five('5poundsoff')).to eq true
-    end
-
-    it 'should return false if the code is incorrect' do
-      expect(@discountcodes.discount_five('wrong code')).to eq false
-    end
-  end
-
-  context '10 pounds off' do
-    it 'should return true if the code is correct and shoppingcarttotal > 50' do
-      expect(@discountcodes.discount_ten('10poundsoff', 51)).to eq true
-    end
-
-    it 'should return false if the code is incorrect' do
-      expect(@discountcodes.discount_ten('wrong code', 100)).to eq false
-    end
-
-    it 'should return false if shoppingcarttotal < 50' do
-      expect(@discountcodes.discount_ten('10poundsoff', 49)).to eq false
-    end
-  end
-
-  context '15 pounds off' do
-    it 'should return true if the code is correct and shoppingcarttotal > 75' do
-      allow(stock).to receive(:check_category).and_return(true)
-      expect(@discountcodes.discount_fifteen('15poundsoff', 76, shoppingcartwithfootwear, stock)).to eq true
-    end
-
-    it 'should return false if the code is incorrect' do
-      allow(stock).to receive(:check_category).and_return(true)
-      expect(@discountcodes.discount_fifteen('wrong code', 100, shoppingcartwithfootwear, stock)).to eq false
-    end
-
-    it 'should return false if shoppingcarttotal < 75' do
-      allow(stock).to receive(:check_category).and_return(true)
-      expect(@discountcodes.discount_fifteen('15poundsoff', 49, shoppingcartwithfootwear, stock)).to eq false
-    end
-
-    it 'should return false if shoppingcart does not contain a footwear item' do
-      allow(stock).to receive(:check_category).and_return(false)
-      expect(@discountcodes.discount_fifteen('15poundsoff', 100, shoppingcartwithoutfootwear, stock)).to eq false
-    end
-  end
-
   context 'discount check' do
     it 'should return 5 if the code is correct' do
       expect(@discountcodes.discount_check('5poundsoff', 100, shoppingcartwithfootwear)).to eq 5
@@ -75,6 +29,36 @@ describe 'DiscountCodes' do
 
     it 'should return 0 if shoppingcarttotal < 50' do
       expect(@discountcodes.discount_check('10poundsoff', 1, shoppingcartwithfootwear)).to eq 0
+    end
+
+    it 'should return 15 if the code is correct and shoppingcarttotal > 75' do
+      allow(stock).to receive(:check_category).and_return(true)
+      expect(@discountcodes.discount_check('15poundsoff', 76, shoppingcartwithfootwear, stock)).to eq 15
+    end
+
+    it 'should return 0 if the code is incorrect' do
+      allow(stock).to receive(:check_category).and_return(true)
+      expect(@discountcodes.discount_check('wrong code', 100, shoppingcartwithfootwear, stock)).to eq 0
+    end
+
+    it 'should return 0 if shoppingcarttotal < 75' do
+      allow(stock).to receive(:check_category).and_return(true)
+      expect(@discountcodes.discount_check('15poundsoff', 49, shoppingcartwithfootwear, stock)).to eq 0
+    end
+
+    it 'should return 0 if shoppingcart does not contain a footwear item' do
+      allow(stock).to receive(:check_category).and_return(false)
+      expect(@discountcodes.discount_check('15poundsoff', 100, shoppingcartwithoutfootwear, stock)).to eq 0
+    end
+
+    it 'should return 15 if shoppingcart only contains mens footwear item' do
+      allow(stock).to receive(:check_category).and_return(true, false)
+      expect(@discountcodes.discount_check('15poundsoff', 100, shoppingcartwithoutfootwear, stock)).to eq 15
+    end
+
+    it 'should return 15 if shoppingcart only contains womens footwear item' do
+      allow(stock).to receive(:check_category).and_return(false, true)
+      expect(@discountcodes.discount_check('15poundsoff', 100, shoppingcartwithoutfootwear, stock)).to eq 15
     end
   end
 end
