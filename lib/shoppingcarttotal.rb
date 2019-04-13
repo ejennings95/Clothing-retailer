0a764f020f12fb84ec5a479c70a4ccaeafae8df9
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-04-13 10:54:53 +0100

    :recycle: refactor clean code of shopping cart total

--- lib/shoppingcarttotal.rb
+++ lib/shoppingcarttotal.rb
@@ -6,8 +6,8 @@ class ShoppingCartTotal
   attr_reader :price, :discount_price, :discountcodes
 
   def initialize(discount_codes = DiscountCodes.new)
-    @price = 0
-    @discount_price = 0
+    @price = '0.00'
+    @discount_price = '0.00'
     @discountcodes = discount_codes
   end
 
@@ -15,17 +15,25 @@ class ShoppingCartTotal
     @price = 0
     shoppingcart.each{ | item | @price += stock.check_price(item) }
     total_price_with_discount_check(code, @price, shoppingcart)
-    @price = two_decimal_places(@price)
   end
 
   private
 
   def total_price_with_discount_check(code, shoppingcarttotal, shoppingcart)
-    if code == nil || @price == 0
-      @discount_price = two_decimal_places(@price)
-    else
-      @discount_price = two_decimal_places(@price - @discountcodes.discount_check(code, shoppingcarttotal, shoppingcart))
-    end
+    (@price == 0) ? set_no_discount : set_discount_price(code, shoppingcarttotal, shoppingcart)
+    set_price
+  end
+
+  def set_price
+    @price = two_decimal_places(@price)
+  end
+
+  def set_no_discount
+    @discount_price = two_decimal_places(@price)
+  end
+
+  def set_discount_price(code, shoppingcarttotal, shoppingcart)
+    @discount_price = two_decimal_places(@price - @discountcodes.discount_check(code, shoppingcarttotal, shoppingcart))
   end
 
   def two_decimal_places(amount)
