dc33c68eb714323e0e014bcd64e56d16bf7211a6
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-04-14 15:25:40 +0100

    :recycle: dry shoppingcart_spec tests

--- spec/shoppingcart_spec.rb
+++ spec/shoppingcart_spec.rb
@@ -7,12 +7,13 @@ describe 'ShoppingCart' do
 
   before(:each) do
     @testshoppingcart = ShoppingCart.new(stock, shoppingcarttotal)
+    allow(stock).to receive(:in_stock?).and_return(true)
+    allow(stock).to receive(:reduce_stock_by_1)
+    allow(stock).to receive(:increase_stock_by_1)
   end
 
   context 'add_to_cart' do
     it 'should increase the cart quantity by 1' do
-      allow(stock).to receive(:in_stock?).and_return(true)
-      allow(stock).to receive(:reduce_stock_by_1)
       allow(stock).to receive(:find_item_quantity_1).and_return("item")
       allow(shoppingcarttotal).to receive(:total_price)
       expect { @testshoppingcart.add_to_cart("item") }.to change { @testshoppingcart.shoppingcart.length }.by 1
@@ -21,17 +22,12 @@ describe 'ShoppingCart' do
 
   context 'remove_from_cart' do
     it 'should decrease the cart quantity by 1' do
-      allow(stock).to receive(:in_stock?).and_return(true)
-      allow(stock).to receive(:reduce_stock_by_1)
       allow(stock).to receive(:find_item_quantity_1).and_return("item")
       allow(shoppingcarttotal).to receive(:total_price)
       expect { @testshoppingcart.add_to_cart("item") }.to change { @testshoppingcart.shoppingcart.length }.by 1
     end
 
     it 'should remove the correct item from the cart' do
-      allow(stock).to receive(:in_stock?).and_return(true)
-      allow(stock).to receive(:increase_stock_by_1)
-      allow(stock).to receive(:reduce_stock_by_1)
       allow(stock).to receive(:find_item_quantity_1).and_return("item", "item2")
       allow(shoppingcarttotal).to receive(:total_price)
       @testshoppingcart.add_to_cart("item")
@@ -42,9 +38,6 @@ describe 'ShoppingCart' do
     end
 
     it 'should remove the item only once if added twice' do
-      allow(stock).to receive(:in_stock?).and_return(true)
-      allow(stock).to receive(:increase_stock_by_1)
-      allow(stock).to receive(:reduce_stock_by_1)
       allow(stock).to receive(:find_item_quantity_1).and_return("item", "item")
       allow(shoppingcarttotal).to receive(:total_price)
       @testshoppingcart.add_to_cart("item")
@@ -56,8 +49,6 @@ describe 'ShoppingCart' do
 
   context 'total price returned from shoppingcarttotal' do
     it 'should pass the cart to the shoppingcarttotal and have a number returned in string format' do
-      allow(stock).to receive(:in_stock?).and_return(true)
-      allow(stock).to receive(:reduce_stock_by_1)
       allow(stock).to receive(:find_item_quantity_1).and_return("item")
       allow(shoppingcarttotal).to receive(:total_price).and_return('10.00')
       @testshoppingcart.add_to_cart("item")
